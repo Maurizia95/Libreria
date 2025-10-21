794ecae8b1c0695589e6626fa59d56e1b6f2a15e
Maurizia95 <mauriziaporcaro@gmail.com>
2025-10-21 21:53:07 +0200

    Modify carrello.java

--- src/main/java/com/libreria/entita/Carrello.java
+++ src/main/java/com/libreria/entita/Carrello.java
@@ -1,5 +1,52 @@
 package com.libreria.entita;
 
-public class Carrello {
+import java.text.DateFormat;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "cart")
+public class Carrello {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name="creation_date", nullable=true) 
+	private DateFormat data_creazione;
+	
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public DateFormat getData_creazione() {
+		return data_creazione;
+	}
+
+	public void setData_creazione(DateFormat data_creazione) {
+		this.data_creazione = data_creazione;
+	}
+
+	
+	public Carrello(DateFormat data_creazione) {
+	
+		setData_creazione(data_creazione);
+	}
+	
+	
+	public Carrello() {}
+	
+	
+	
 }
+
