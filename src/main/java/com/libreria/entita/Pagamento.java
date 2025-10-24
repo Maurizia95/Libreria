a515f963f61305b10a656c7efb92bd5521d3c993
Maurizia95 <mauriziaporcaro@gmail.com>
2025-10-24 17:59:06 +0200

    Modify Cliente.java

--- src/main/java/com/libreria/entita/Pagamento.java
+++ src/main/java/com/libreria/entita/Pagamento.java
@@ -1,12 +1,12 @@
 package com.libreria.entita;
 import java.text.DateFormat;
 
-import com.libreria.entita.Table;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 @jakarta.persistence.Table(name= "payments")
