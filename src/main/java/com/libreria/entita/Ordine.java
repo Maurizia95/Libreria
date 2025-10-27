250481d630493c6ab095b32c9ad15d5c649efbd5
Maurizia95 <mauriziaporcaro@gmail.com>
2025-10-27 20:24:46 +0100

    Ordine

--- src/main/java/com/libreria/entita/Ordine.java
+++ src/main/java/com/libreria/entita/Ordine.java
@@ -1,58 +1,52 @@
 package com.libreria.entita;
 
-import java.text.DateFormat;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import java.time.LocalDate;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name= "orders")
+@Table(name = "orders")
 public class Ordine {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
-	@Column(name="order_date", nullable=false)
-	private DateFormat data_ordine;
-	
-	@Column(name="order_status", nullable=false)
-	private String stato_ordine;
-	
-	//getter e setter
-	public Integer getId() {
-		return id;
-	}
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	public DateFormat getData_ordine() {
-		return data_ordine;
-	}
+    @Column(name = "order_date", nullable = false)
+    private LocalDate dataOrdine;
 
-	public void setData_ordine(DateFormat data_ordine) {
-		this.data_ordine = data_ordine;
-	}
+    @Column(name = "order_status", nullable = false)
+    private String statoOrdine;
 
-	public String getStato_ordine() {
-		return stato_ordine;
-	}
+    // Getter e Setter
+    public Integer getId() {
+        return id;
+    }
 
-	public void setStato_ordine(String stato_ordine) {
-		this.stato_ordine = stato_ordine;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	//costruttori
-	public Ordine(DateFormat data_ordine, String stato_ordine) {
-		setData_ordine(data_ordine);
-		setStato_ordine(stato_ordine);
-	}
-	
-	public Ordine() {}
+    public LocalDate getDataOrdine() {
+        return dataOrdine;
+    }
 
+    public void setDataOrdine(LocalDate dataOrdine) {
+        this.dataOrdine = dataOrdine;
+    }
+
+    public String getStatoOrdine() {
+        return statoOrdine;
+    }
+
+    public void setStatoOrdine(String statoOrdine) {
+        this.statoOrdine = statoOrdine;
+    }
+
+    // Costruttori
+    public Ordine(LocalDate dataOrdine, String statoOrdine) {
+        this.dataOrdine = dataOrdine;
+        this.statoOrdine = statoOrdine;
+    }
+
+    public Ordine() {}
 }
