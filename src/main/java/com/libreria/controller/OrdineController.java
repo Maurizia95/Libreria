4fc539d51c54abc8318047c3bff6b85a8eae5826
Maurizia95 <mauriziaporcaro@gmail.com>
2025-10-27 20:48:52 +0100

    OrdineController

--- src/main/java/com/libreria/controller/OrdineController.java
+++ src/main/java/com/libreria/controller/OrdineController.java
@@ -8,27 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.libreria.entita.Ordine;
+import com.libreria.service.OrdineService;
 
 @RestController
 @RequestMapping("/ordine")
 public class OrdineController {
-@Autowired OrdineService ordineService;
 
-/*
-@GetMapping("/Test") //marcatore aggiuntivo per accedere al metodo (/ordine/Test)
-public void Test() {
-	System.out.println("test"); 
-	}*/
+    @Autowired
+    private OrdineService ordineService;
 
-@PostMapping("/create")
-public ResponseEntity <Ordine> insert(RequestBody Ordine ordine){
-	
-	if(data_ordine==null || stato_ordine==null) {
-		return ResponseEntity.badRequest().build();
-	}
-	
-	ordineService.save(ordine);
-	return ResponseEntity.ok(ordine);
+    @PostMapping("/create")
+    public ResponseEntity<Ordine> insert(@RequestBody Ordine ordine) {
+
+        if (ordine.getDataOrdine() == null || ordine.getStatoOrdine() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        ordineService.save(ordine);
+        return ResponseEntity.ok(ordine);
+    }
 }
 
-}
