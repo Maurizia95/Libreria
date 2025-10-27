51074e26f622cb4f8e638e0a97e5c6f4e493159a
Maurizia95 <mauriziaporcaro@gmail.com>
2025-10-27 17:47:29 +0100

    Init LibroRepository

---
+++ src/main/java/com/libreria/repository/LibroRepository.java
@@ -0,0 +1,11 @@
+package com.libreria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.libreria.entita.Libro;
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Integer> { //interfaccia che ci permette di comunicare con il database per fare delle query tramite metodi creati con parole chiavi
+	
+
+}
