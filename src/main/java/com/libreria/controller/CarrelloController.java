40948854cef1127d564435f1bc834db14012377a
Maurizia95 <mauriziaporcaro@gmail.com>
2025-10-27 21:19:29 +0100

    Controller.java

---
+++ src/main/java/com/libreria/controller/CarrelloController.java
@@ -0,0 +1,27 @@
+package com.libreria.controller;
+
+import java.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.libreria.entita.Carrello;
+import com.libreria.entita.Ordine;
+import com.libreria.service.CarrelloService;
+
+@RestController
+@RequestMapping("/carrello")
+public class CarrelloController {
+
+    @Autowired
+    private CarrelloService carrelloService;
+
+    @PostMapping("/carrello")
+    public ResponseEntity<Carrello> insert(@RequestBody Carrello carrello) {
+        if (carrello == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        carrelloService.save(carrello);
+        return ResponseEntity.ok(carrello);
+    }
+}
