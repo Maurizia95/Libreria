63fe80ecc74f152872c450d1291b9e63cf893d29
Maurizia95 <mauriziaporcaro@gmail.com>
2025-11-01 09:23:07 +0100

    up

--- src/main/java/com/libreria/service/LibroService.java
+++ src/main/java/com/libreria/service/LibroService.java
@@ -1,5 +1,7 @@
 package com.libreria.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,11 @@ public class LibroService {
 	
 	public Libro save(Libro libro) { //metodo che salva una entità libro nel database 
 		return libroRepository.save(libro); //tramite il metodo save del repository
+	}
+
+	public Optional<Libro> findById(int id) { //Optional potrebbe o non potrebbe ritornare un oggetto Libro
+		
+		return libroRepository.findById(id);
 	}
 	
 	
